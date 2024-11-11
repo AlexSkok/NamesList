ceea34a15ace63c0e7a7775ca590051afa115bf5
Alex <olexiyskok@gmail.com>
2024-11-11 20:41:02 +0200

    create README

---
+++ README.md
@@ -0,0 +1,1 @@
+<h1>Names List APP</h1>
--- src/main/java/app/UIOperator.java
+++ src/main/java/app/UIOperator.java
@@ -1,2 +1,7 @@
-package app;public class UIOperator {
+package app;
+
+public class UIOperator {
+    public void getOutput(String output) {
+        System.out.println(output);
+    }
 }
